b6515f16c838429dc984271f23b9a08145836aa5
Chaitanya Agrawal <agrawac@amazon.com>
2024-01-07 22:07:00 -0800

    Updated the script for linux and windows setup

--- machine/win.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Windows
-mkdir win;
-cd win;
-curl -L https://raw.githubusercontent.com/carlospolop/PEASS-ng/master/winPEAS/winPEASps1/winPEAS.ps1 -o winPEAS.ps1;
-
--- scripts/linux.sh
+++ scripts/linux.sh
@@ -7,4 +7,8 @@ cd linux;
 curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh -o linpeas.sh;
 curl -L https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy32 -o pspy32;
 curl -L https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64 -o pspy64;
+
+# AD
+pip3 install certipy-ad
+
 cd ../;
